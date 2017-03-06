d07c289224a5f6d1e56870a70333cede60439fe2
AlexSemenov <aleks-boy97@mail.ru>
2017-03-07 01:44:54 +0300

    Осталось сделать список всех городов и субъектов

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Lab1
 {
@@ -20,9 +23,50 @@ namespace Lab1
     /// </summary>
     public partial class MainWindow : Window
     {
+        //private string myDirectory = string.Empty;    //    инициализируем директорию
+        //private XmlDocument MyDoc = null;    //    БД
+        //DataTable Citytable = null;    //    таблицу с данными
+        //DataSet Cities = null;    //    данные
+
         public MainWindow()
         {
+            //myDirectory = Directory.GetCurrentDirectory();        //присваиваем значение текущей директории
+            // ЗДЕСЬ МОЖЕТ БЫТЬ ОШИБКА
+            //using (StreamReader streamreader = new StreamReader(/*myDirectory + */@"C:\Users\HOOLIGAN\Documents\Visual Studio 2015\Projects\Lab1\Lab1\rocid.xml", System.Text.Encoding.UTF8))
+            //{
+                //Cities = new DataSet();
+                //Cities.ReadXml(streamreader, XmlReadMode.Auto);
+                //Citytable = Cities.Tables[0];    // присваиваем данные БД таблицы
+            //}
             InitializeComponent();
+        }
+
+        private void Submit_Form(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Reset_Form(object sender, RoutedEventArgs e)
+        {
+            /*foreach (Control control in MainWindow.Controls)
+            {
+                if (control is TextBox)
+                {
+                    TextBox textBox = (TextBox)control;
+                    textBox.Text = null;
+                }
+
+                if (control is ComboBox)
+                {
+                    ComboBox comboBox = (ComboBox)control;
+                    if (comboBox.Items.Count > 0)
+                        comboBox.SelectedIndex = 0;
+                }
+
+                first.IsChecked = true;
+                second.IsChecked = false;
+                third.IsChecked = false;
+            }*/
         }
     }
 }
